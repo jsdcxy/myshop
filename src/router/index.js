0b57582ddda65fa2978cc8d3ea226802e1e2d415
jsdcxy <37364799+jsdcxy@users.noreply.github.com>
2018-04-10 10:52:59 +0800

    路由

--- src/router/index.js
+++ src/router/index.js
@@ -20,6 +20,21 @@ export default new Router({
       path: '/products/:category',
       name: 'product-list-by-category',
       component: () => import('@/pages/products/index.vue')
+    },
+    {
+      path: '/information',
+      name: 'product-information',
+      component: () => import('@/pages/information/index.vue')
+    },
+    {
+      path: '/commodity/:category',
+      name: 'product-commodity-category',
+      component: () => import('@/pages/commodity/index.vue')
+    },
+    {
+      path: '/cart',
+      name: 'cart-list',
+      component: () => import('@/pages/cart/index.vue')
     }
   ]
 })
